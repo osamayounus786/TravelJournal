d705f5ebabbe009c8865448a236d633ad8a3681e
Osama M.Younus <osamayounus541@gmail.com>
2022-12-01 15:07:30 +0500

    my travel initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+    return(
+
+<h1>Hello header</h1>
+    )
+}
+export default Header;
---
+++ src/components/main.jsx
@@ -0,0 +1,7 @@
+function Main(){
+    return(
+        <h1>Hello main </h1>
+    )
+}
+
+export default Main;
